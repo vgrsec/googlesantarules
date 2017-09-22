f3394bff7f36a6c750d15ce27f4c3adfc0cd61de
Valentine Reid <val.reid@pokitdok.com>
2017-09-21 22:00:48 -0400

    Google Chrome

---
+++ googlechrome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This is Google Chrome as of 61.0.3163.79
+sudo santactl rule --whitelist --certificate --sha256 345a8e098bd04794aaeefda8c9ef56a0bf3d3706d67d35bc0e23f11bb3bffce5
+sudo santactl rule --whitelist --certificate --sha256 33b9aee3b089c922952c9240a40a0daa271bebf192cf3f7d964722e8f2170e48
