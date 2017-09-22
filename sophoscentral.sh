7fd9919b5383149f6424b04e62729bde51a02eb6
Valentine Reid <val.reid@pokitdok.com>
2017-09-21 22:16:41 -0400

    VMWare Tools and Sophos Central

---
+++ sophoscentral.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is Sophos Central as of 20170921
+#
+sudo santactl rule --whitelist --certificate --sha256 740c71e654da5f16956cae04165c9d5cb0839e9cf870f560ce8fba9f36d78b7c
+sudo santactl rule --whitelist --certificate --sha256 d747c16c9107fc36dbdbb662aa3324b18385fb55f2123d95612ed717e20515fc
+
---
+++ vmware.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This is for VMWare Tools as of 20170921
+sudo santactl rule --whitelist --certificate --sha256 6adf85cc7bb16eeb9a40ff14622452ff74fcbc3c77df0dc264a02f659bf4fbcb
